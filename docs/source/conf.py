c70cb3da0063145be930a93a05ba404800720873
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-11-02 19:29:24 -0700

    docs: Switch to furo theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -188,6 +188,7 @@ default_role = 'math'
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
+pygments_dark_style = "monokai"
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -199,8 +200,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sage'
-html_theme_path = ['../themes']
+html_theme = 'furo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
